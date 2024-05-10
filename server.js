ce0f9cc2ab8a9a4d38f745e479abe5345b0d753e
priyanshu09143 <vishal9772345@gmail.com>
2024-05-10 12:29:26 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import {createServer} from "http"
+import {Server} from "socket.io"
+import { configDotenv } from 'dotenv';
+
+const app = express()
+const server = createServer(app)
+configDotenv({
+    path : "./config.env"
+})
+const io = new Server(server,{
+    cors : {
+        origin : process.env.ORIGIN,
+
+    }
+})
+app.get("/",(req,res)=>{
+    res.send("Hello World socket")
+})
+
+let connectedUser = []
+io.on('connection', (socket) => {
+    console.log("user is connected" , socket.id)
+    if(connectedUser.length === 0){
+        connectedUser.push(socket.id)
+    }
+    socket.emit("connected", connectedUser)
+    socket.on("click",(btn)=>{
+        socket.broadcast.emit("btnClicks",(`Button ${btn}`))
+    })
+
+    socket.on("position" , (da)=>{
+        io.emit("newPos" , da)
+    })
+      socket.on("clickPos",(data)=>{
+        socket.broadcast.emit("newclick" , data)
+      })
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        if(socket.id === connectedUser[0]){
+            connectedUser.pop()
+        }
+      });
+  });
+
+server.listen(8000, ()=>{
+    console.log("server Is Running At Port 8000")
+})
